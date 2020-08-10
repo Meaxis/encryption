aa9ff330459c394fd485c6dfa64785be2d7fa3cc
Meaxis <37759268+Meaxis@users.noreply.github.com>
2020-08-10 16:53:48 +0200

    Create mmc1.php

---
+++ mmc1.php
@@ -0,0 +1,71 @@
+<?php
+	if (isset($_POST['tmode']) && $_POST['tmode'] == "tonormal" && isset($_POST['text']))
+	{
+		$increment = 0;
+		$text = $_POST['text'];
+
+		foreach(range('A','Z') as $letter) 
+		{
+			$increment++;
+			$text = str_replace($letter, "{".$increment."}", $text);   
+		}
+
+		foreach(range('a','z') as $letter) 
+		{
+			$increment++;
+			$text = str_replace($letter, "{".$increment."}", $text);   
+		}
+
+		foreach ($conversiontable as $key => $value) {
+			$text = str_replace($key, $value, $text);
+		}
+
+		echo "<h2>Result:</h2><br><code>".$text."</code>";
+	}
+
+	elseif (isset($_POST['tmode']) && $_POST['tmode'] == "tocipher" && isset($_POST['text']))
+	{
+		$text = $_POST['text'];
+		$increment = 0;
+		foreach(range('N','Z') as $letter) 
+		{
+			$increment++;
+			$text = str_replace($letter, "{".$increment."}", $text);   
+		}
+
+		foreach(range('A','M') as $letter) 
+		{
+			$increment++;
+			$text = str_replace($letter, "{".$increment."}", $text);   
+		}
+
+		foreach(range('n','z') as $letter) 
+		{
+			$increment++;
+			$text = str_replace($letter, "{".$increment."}", $text);   
+		}
+
+		foreach(range('a','m') as $letter) 
+		{
+			$increment++;
+			$text = str_replace($letter, "{".$increment."}", $text);   
+		}
+
+		$increment = 0;
+
+		foreach(range('A','Z') as $letter) 
+		{
+			$increment++;
+			$text = str_replace("{".$increment."}", $letter, $text);   
+		}
+
+		foreach(range('a','z') as $letter) 
+		{
+			$increment++;
+			$text = str_replace("{".$increment."}", $letter, $text);   
+		}
+
+		echo "<h2>Result:</h2><br><code>".$text."</code>";
+	}
+
+?>
